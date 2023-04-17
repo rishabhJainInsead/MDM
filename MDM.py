52cb159808aad2fc99ea477369c48506cee38882
rishabhJainInsead <131004597+rishabhJainInsead@users.noreply.github.com>
2023-04-17 17:50:06 +0200

    Removed Commentary

--- MDM.py
+++ MDM.py
@@ -7,38 +7,6 @@ from StepThree import stepThree
 from StepFour import stepFour
 from StepFive import stepFive
 
-
-'''basic layout
-
-Decision to make:
-    options: A, B, C, D, E
-
-Go through decision process checks
-    1. Have we considered our fundamental objectives? What are we trying to
-    achieve?
-    2. Have we expanded our view and considered multiple perspectives? Have we thought creatively and considered many alternatives, beyond
-    the most evident ones?
-    3. What is missing from our analysis? Where is the gorilla? What is it that
-    we do not know? What do we wish we knew? What additional information
-    would help inform our decision? How can we get this information?
-    4. Have we sought for evidence that contradicts our beliefs?
-    5. Have we reality-tested our assumptions? What if we are wrong?
-    6. Are we doing what we believe works or what really works? How can we test our ideas and beliefs with some quick experiments?
-    7. Is it a true pattern or a cinnamon roll?
-    8. Have we considered all relevant uncertainties - even those that appear highly improbable?
-    9. What are potential coconuts/black swans? What are the main threats? Opportunities?
-    10. Have we included redundancy in our system to account for (un)known unknowns?
-    11. How confident or optimistic are we in our assessment of the situation? If highly confident, go back to questions 1-13.
-    12. Does irrational escalation of commitment play a role in our decision? Are we overcommitted?
-    13. Groups: Who has what kind of knowledge? Why do we all agree? Could it be because we are all similarly wrong?
-    14. Groups: Has there been any/enough disagreement and divergent thinking in the group? How can we further explore individual points of
-    view in the group?
-    15. How does our decision align with our values and ethical considerations? To which extent is this decision ethical? Unethical? Can
-    we think of other options that would be more in line with our values?
-    16. What am I feeling? How might emotions be influencing our decision? Is it the right moment to make any decision? Are we putting unnecessary time
-    constraints on our thinking process which increase emotions? Have we given ourselves enough time to consider all alternatives?
-'''
-
 def MDM():
     print_green("""-------------------------------------------------
 Welcome to your computer-aided decision making toolkit!""")
